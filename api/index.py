5479a6081168bbad99f96aefdc719918bfa9beed
zqh0421 <zqh0421@hotmail.com>
2024-09-18 21:23:24 +0000

    Update

--- api/index.py
+++ api/index.py
@@ -5,3 +5,9 @@ app = FastAPI()
 @app.get("/api/hello")
 async def read_hello():
     return {"message": "Hello from FastAPI!"}
+
+app = FastAPI()
+
+@app.get("/api/python")
+def hello_world():
+    return {"message": "Hello World"}
