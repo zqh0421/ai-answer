d069d6bc2c2947cba1052eed3b192746a69d3083
zqh0421 <zqh0421@hotmail.com>
2024-09-25 18:02:25 +0000

    Update dubugging

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 from pydantic import BaseModel
 from fastapi import FastAPI, Depends
 from typing_extensions import Annotated
-from langchain_community.document_loaders import PyPDFLoader
+# from langchain_community.document_loaders import PyPDFLoader
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from .chat import judge_answer
@@ -45,27 +45,33 @@ def embed(embedModel: EmbedModel, settings: Annotated[Settings, Depends(get_sett
     if not os.path.isfile(file_path):
         raise FileNotFoundError(f"The file {file_path} does not exist.")
 
-    loader = PyPDFLoader(file_path)
+    # loader = PyPDFLoader(file_path)
 
-    docs = loader.load()
+    # docs = loader.load()
 
     # print(docs[0].metadata)
 
-    contents = []
-    for i in range(len(docs)):
-        contents.append(docs[i].page_content)
-
-    # Get slide vectors
-    content_vectors = embed_slide(contents, settings)
-
-    # Retrieve the most relevant reference based on cosine similarity
-    page_number, best_match_content = retrieve_reference(text_vector, content_vectors, contents)
-
-    # retrieve related reference from given slide and return the page
+    # contents = []
     return {
         "result": {
-            "content": f"{best_match_content}",
-            "page_number": page_number
+            "content": f"test_contenttt",
+            "page_number": 0
         }
     }
+    # for i in range(len(docs)):
+    #     contents.append(docs[i].page_content)
+
+    # # Get slide vectors
+    # content_vectors = embed_slide(contents, settings)
+
+    # # Retrieve the most relevant reference based on cosine similarity
+    # page_number, best_match_content = retrieve_reference(text_vector, content_vectors, contents)
+
+    # # retrieve related reference from given slide and return the page
+    # return {
+    #     "result": {
+    #         "content": f"{best_match_content}",
+    #         "page_number": page_number
+    #     }
+    # }
 
