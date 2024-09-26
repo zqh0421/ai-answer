534bec19f0c7b33cae8b780bcec0ec59ceb209e4
zqh0421 <zqh0421@hotmail.com>
2024-09-26 21:51:01 +0000

    Update docker

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+# Use an official Node.js image as the base image
+FROM node:20.17.0 AS node-builder
+
+# Set working directory
+WORKDIR /.
+
+# Copy the package.json and package-lock.json files
+COPY package*.json ./
+
+COPY ./requirements.txt ./
+
+COPY . .
+RUN ls -la /app
+
+# Install dependencies
+RUN npm install -g pnpm
+RUN pnpm install
+
+# Use an official Python image as the base image
+FROM python:3.12.6 AS python-builder
+
+# Copy the rest of the app
+COPY --from=node-builder . .
+RUN ls -la /app
+
+# # Build the Next.js app
+RUN NEXT_PUBLIC_ESLINT_DISABLED=truepnpm build
+
+# Expose the ports that the project runs on
+EXPOSE 3000 8000
+
+# Start the Next.js app
+CMD ["pnpm", "dev"]
--- api/index.py
+++ api/index.py
@@ -3,9 +3,9 @@ from openai import OpenAI
 from pydantic import BaseModel
 from fastapi import FastAPI, Depends
 from typing_extensions import Annotated
-# from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+import numpy as np
 from .chat import judge_answer
 from .config import Settings, get_settings
 from .embedding import create_embedding, embed_slide, combine_embedding, retrieve_reference
@@ -45,33 +45,33 @@ def embed(embedModel: EmbedModel, settings: Annotated[Settings, Depends(get_sett
     if not os.path.isfile(file_path):
         raise FileNotFoundError(f"The file {file_path} does not exist.")
 
-    # loader = PyPDFLoader(file_path)
+    loader = PyPDFLoader(file_path)
 
-    # docs = loader.load()
+    docs = loader.load()
 
-    # print(docs[0].metadata)
+    print(docs[0].metadata)
 
-    # contents = []
-    return {
-        "result": {
-            "content": f"test_contenttt",
-            "page_number": 0
-        }
-    }
-    # for i in range(len(docs)):
-    #     contents.append(docs[i].page_content)
-
-    # # Get slide vectors
-    # content_vectors = embed_slide(contents, settings)
-
-    # # Retrieve the most relevant reference based on cosine similarity
-    # page_number, best_match_content = retrieve_reference(text_vector, content_vectors, contents)
-
-    # # retrieve related reference from given slide and return the page
+    contents = []
     # return {
     #     "result": {
-    #         "content": f"{best_match_content}",
-    #         "page_number": page_number
+    #         "content": f"test_contenttt",
+    #         "page_number": 0
     #     }
     # }
+    for i in range(len(docs)):
+        contents.append(docs[i].page_content)
+
+    # Get slide vectors
+    content_vectors = embed_slide(contents, settings)
+
+    # Retrieve the most relevant reference based on cosine similarity
+    page_number, best_match_content = retrieve_reference(text_vector, content_vectors, contents)
+
+    # retrieve related reference from given slide and return the page
+    return {
+        "result": {
+            "content": f"{best_match_content}",
+            "page_number": page_number
+        }
+    }
 
